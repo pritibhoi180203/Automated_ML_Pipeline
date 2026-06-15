ccaa5d3e881f4942b52d0bb935916dcb7c907e52
pritibhoi180203 <pritibhoi180203@gmail.com>
2026-06-15 12:44:49 +0530

    Updated CI Pipeline

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,16 +2,30 @@ import pandas as pd
 import joblib
 
 from sklearn.metrics import accuracy_score
-
-model = joblib.load("models/model.pkl")
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/titanic.csv")
 
-df.fillna(0,inplace=True)
+df = df[["Pclass","Sex","Age","Fare","Survived"]]
 
-X = df[['Pclass','Age','Fare']]
-y = df['Survived']
+df["Age"].fillna(df["Age"].median(), inplace=True)
 
-pred = model.predict(X)
+df["Sex"] = df["Sex"].map({
+    "male":0,
+    "female":1
+})
 
-print(accuracy_score(y,pred))
+X = df.drop("Survived",axis=1)
+y = df["Survived"]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+model = joblib.load("models/model.pkl")
+
+pred = model.predict(X_test)
+
+acc = accuracy_score(y_test,pred)
+
+print("Accuracy:",acc)
