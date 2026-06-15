9be6edb4f3930d60a211549ea24db9963ee37512
pritibhoi180203 <pritibhoi180203@gmail.com>
2026-06-15 10:56:00 +0530

    Added prediction API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 
 app = FastAPI()
 
+model = joblib.load("models/model.pkl")
+
+class Passenger(BaseModel):
+    Pclass: int
+    Sex: int
+    Age: float
+    Fare: float
+
 @app.get("/")
 def home():
-    return {"message": "Version 2 deployed successfully"}
+    return {
+        "message": "Titanic Survival Prediction API"
+    }
+
+@app.post("/predict")
+def predict(passenger: Passenger):
+
+    features = [[
+        passenger.Pclass,
+        passenger.Sex,
+        passenger.Age,
+        passenger.Fare
+    ]]
+
+    prediction = model.predict(features)
+
+    return {
+        "prediction": int(prediction[0]),
+        "result": "Survived" if prediction[0] == 1 else "Not Survived"
+    }
--- src/train.py
+++ src/train.py
@@ -7,17 +7,31 @@ from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv("data/titanic.csv")
 
-df.fillna(0,inplace=True)
+df = df[['Pclass','Sex','Age','Fare','Survived']]
 
-X = df[['Pclass','Age','Fare']]
+df['Age'].fillna(df['Age'].median(), inplace=True)
+
+df['Sex'] = df['Sex'].map({
+    'male': 0,
+    'female': 1
+})
+
+X = df[['Pclass','Sex','Age','Fare']]
 y = df['Survived']
 
-X_train,X_test,y_train,y_test = train_test_split(
-X,y,test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
 
-model.fit(X_train,y_train)
+model.fit(X_train, y_train)
 
 os.makedirs("models", exist_ok=True)
 
