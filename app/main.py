966e53f760e9090c04d35d86da0c15f7b8dda6a7
pritibhoi180203 <pritibhoi180203@gmail.com>
2026-06-15 08:09:10 +0530

    Added FastAPI app

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ import joblib
 
 app = FastAPI()
 
-model = joblib.load("models/model.pkl")
-
 @app.get("/")
 def home():
-    return {"message":"Model Running"}
+    return {"message": "Automated ML Pipeline Running Successfully"}
